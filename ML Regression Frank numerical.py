341411eadf6de8ddb259fe2d2841db0b9dd588c7
fesan81 <fesan818181@gmail.com>
2019-09-03 09:55:33 +0200

    Update ML Regression Frank numerical.py

--- ML Regression Frank numerical.py
+++ ML Regression Frank numerical.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[22]:
-
-
 import numpy as np # linear algebra
 import pandas as pd # 
 import matplotlib.pyplot as plt
@@ -16,53 +13,26 @@ from sklearn.metrics import accuracy_score, confusion_matrix, classification_rep
 from sklearn.model_selection import cross_val_score
 from sklearn.preprocessing import LabelEncoder, StandardScaler
 from sklearn.pipeline import Pipeline
-
 from sklearn.model_selection import train_test_split, KFold, cross_validate
 from sklearn.metrics import accuracy_score
-
-
-# In[2]:
-
 
 normalized = pd.read_csv('normalized.csv', delimiter=',')
 #This file contains the information of the DB without the comments of the causes 
 normalized.head()
 
 
-# In[3]:
-
-
-normalized.info()
-
-
-# First model: Only numerical columns
-
-# In[4]:
-
-
 # Select numeric columns only
 numeric_cols = [cname for cname in normalized.columns if normalized[cname].dtype in ['int64', 'float64']]
 X = normalized[numeric_cols].copy()
-
-
-# In[6]:
-
 
 y = normalized.Amount
 X.drop(['Amount'], axis=1, inplace=True)
 
 
-# In[7]:
-
-
 from sklearn.model_selection import train_test_split
-
 X_train_full, X_valid_full, y_train, y_valid = train_test_split(X, y, 
                                                                 train_size=0.8, test_size=0.2,
                                                                 random_state=0)
-
-
-# In[12]:
 
 
 from sklearn.ensemble import RandomForestRegressor
@@ -89,9 +59,6 @@ scores = -1 * cross_val_score(my_pipeline, X, y,
 print("Average MAE score:", scores.mean())
 
 
-# In[14]:
-
-
 def get_score(n_estimators):
     """Return the average MAE over 3 CV folds of random forest model.
         Keyword argument:
@@ -108,26 +75,15 @@ def get_score(n_estimators):
     return scores.mean()
 
 
-# In[17]:
-
-
 results = {}
 for i in range (50,750,50):
     results[i] = get_score(i) 
 
 
-# In[18]:
-
-
 plt.plot(results.keys(), results.values())
 plt.show()
 
-
-# In[20]:
-
-
 n_estimators_best = min(results, key=results.get)
 print(n_estimators_best)
-
 
 # best model: RandomForestRegressor(n_estimators=50, random_state=0)
